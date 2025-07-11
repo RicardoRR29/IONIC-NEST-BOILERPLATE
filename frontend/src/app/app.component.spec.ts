b0afa51bae91d3c11b9e678417dd669189bfe3c7
Ricardo Rocha Ribeiro <r3ifes@gmail.com>
2025-07-11 13:33:01 -0300

    fix frontend unit tests

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   it('should create the app', async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [HttpClientTestingModule, AppComponent],
       providers: [provideRouter([])]
     }).compileComponents();
     
--- frontend/src/app/auth/auth.page.spec.ts
+++ frontend/src/app/auth/auth.page.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, NavController } from '@ionic/angular';
 import { AuthPage } from './auth.page';
 import { AuthService } from '../services/auth.service';
 
@@ -20,7 +20,10 @@ describe('AuthPage', () => {
     auth = new AuthServiceMock();
     await TestBed.configureTestingModule({
       imports: [IonicModule, ReactiveFormsModule, RouterTestingModule, AuthPage],
-      providers: [{ provide: AuthService, useValue: auth }],
+      providers: [
+        { provide: AuthService, useValue: auth },
+        { provide: NavController, useValue: { navigateRoot: () => {}, navigateForward: () => {}, navigateBack: () => {} } },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AuthPage);
