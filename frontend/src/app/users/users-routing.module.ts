5ec243834d046f299991b50195c9e359dd8a0950
Ricardo Rocha Ribeiro <r3ifes@gmail.com>
2025-07-11 12:27:31 -0300

    fix(users): update imports after move

--- frontend/src/app/users/users-routing.module.ts
+++ frontend/src/app/users/users-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UsersPage } from './users.page';
+import { UsersPage } from './pages/users.page';
 
 const routes: Routes = [{ path: '', component: UsersPage }];
 
